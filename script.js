7f1443beceb9fe68a6d2fb471da26de0be00365e
justcodingman <joginder9868@outlook.com>
2021-06-06 17:17:48 +0530

    modified:   index.html 	modified:   script.js     completed:  drag & drop API functionality

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
 // Item Lists
-const itemLists = document.querySelectorAll(".drag-item-list");
+const listColumns = document.querySelectorAll(".drag-item-list");
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -21,6 +21,7 @@ let listArrays = [];
 // Drag Functionality
 
 let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -110,6 +111,28 @@ function updateDOM() {
 function drag(e) {
   draggedItem = e.target;
   console.log("draggedItem", draggedItem);
+}
+// Column Allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// When Item Enters Column Area
+function dragEnter(column) {
+  listColumns[column].classList.add("over");
+  currentColumn = column;
+}
+
+//Dropping Item in column
+function drop(e) {
+  e.preventDefault();
+  // Remove Background color padding
+  listColumns.forEach((column) => {
+    column.classList.remove("over");
+  });
+  //Add Item to column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
 }
 
 // On load
