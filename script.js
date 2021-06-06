c5836a08326bc9378d0d062d7cfec6c98c34f5c2
justcodingman <joginder9868@outlook.com>
2021-06-06 18:43:16 +0530

    modified:   index.html 	modified:   script.js     Done:       Add item functionality

--- script.js
+++ script.js
@@ -110,6 +110,30 @@ function updateDOM() {
   updateSavedColumns();
 }
 
+// Add to column List , Reset Text Box
+function addToColumn(column) {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = "";
+  updateDOM();
+}
+
+// show Add item input box
+function showInputBox(column) {
+  addBtns[column].style.visibility = "hidden";
+  saveItemBtns[column].style.display = "flex";
+  addItemContainers[column].style.display = "flex";
+}
+
+//Hide Item Input box
+function hideInputBox(column) {
+  addBtns[column].style.visibility = "visible";
+  saveItemBtns[column].style.display = "none";
+  addItemContainers[column].style.display = "none";
+  addToColumn(column);
+}
+
 // Allows array to reflect Drag and drop items
 function rebuildArrays() {
   backlogListArray = [];
@@ -145,7 +169,7 @@ function dragEnter(column) {
   listColumns[column].classList.add("over");
   currentColumn = column;
 }
-``
+``;
 //Dropping Item in column
 function drop(e) {
   e.preventDefault();
